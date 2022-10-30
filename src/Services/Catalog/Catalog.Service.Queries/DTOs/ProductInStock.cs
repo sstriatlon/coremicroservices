f6e2fa7f559530605998ae19f31dd2d8745570db
SANTIAGO SUAREZ <SE38050@grupo.ypf.com>
2022-10-30 14:24:25 -0300

    Added DTOs folder for db reading and Common, Mapping and Paging projects

---
+++ src/Services/Catalog/Catalog.Service.Queries/DTOs/ProductInStock.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Catalog.Service.Queries.DTOs
+{
+    public class ProductInStockDto
+    {
+        public int ProductsInStockId { get; set; }
+        public int ProductId { get; set; }   
+        public int Stock { get; set; }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.Service.Queries/ProductQueryService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Catalog.Persistance.Database;
+
+namespace Catalog.Service.Queries
+{
+    public class ProductQueryService
+    { 
+        private readonly ApplicationDbContext _context;
+        public ProductQueryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
